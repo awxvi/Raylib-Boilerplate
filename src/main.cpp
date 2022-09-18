36ab5fdcb8b5e81eb16c602263337cecd90a9b17
awxvi <arcticwx@gmail.com>
2022-09-18 22:55:41 +0100

    various fixes and updates

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,6 @@
 
 #include <iostream>
 
-void confirm_exit() {
-
-    ClearBackground( LIGHTGRAY );
-
-    DrawText("Are you sure you want to exit program? [Y/N]", ( ( GetScreenWidth() / 2 ) / 2 ) - 98, ( ( GetScreenHeight() / 2 ) / 2 ) - 15, 30, BLACK);
-
-    if ( IsKeyPressed( KEY_Y ) ) { CloseWindow(); }
-    else if ( IsKeyPressed( KEY_N ) ) { /* set state equal to the resumed game */ }
-    
-}
-
 void initialize() {
 
     const double screenWidth = 1000;
@@ -20,16 +9,14 @@ void initialize() {
 
     InitWindow(screenWidth, screenHeight, "clicker");
 
-    SetExitKey(KEY_NULL); // To provide confirm exit screen
+    // SetExitKey(KEY_NULL); if you want a custom exit screen
 
     while ( !WindowShouldClose() ) {
 
-        if ( IsKeyPressed(KEY_ESCAPE ) ) {
-            confirm_exit();
-        }
-
         BeginDrawing();
+
             ClearBackground( LIGHTGRAY );
+
         EndDrawing();
         
     }
@@ -40,7 +27,7 @@ void initialize() {
 
 int main( void ) {
     
-    SetTargetFPS(100);
+    SetTargetFPS( 69420 ); // up to you, i recommend trying to implement delta time instead of frame rate limiting though.
 
     initialize();
     
